60fe0252aa8244a6414bd6919b659b35dba5283e
Felipe Pavan <felipepavanm@gmail.com>
2025-02-27 19:41:29 -0300

    adding configuration files

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Garante que o Vercel sirva corretamente os arquivos estáticos
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true, // Redireciona todas as rotas para index.html
+  },
 })
 
 
